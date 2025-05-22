d995b7c183d3f35d8164529bb970c261eb2954fb
Cosimo0C <cosimocrupi01@gmail.com>
2025-05-22 17:07:38 +0200

    S10-L4

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,9 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
-import { describe } from "vitest";
-import Welcome from "../components/Welcome";
 test("Welcome is correctly assembled", () => {
-  render(<Welcome />);
+  render(<App />);
   const welcome = screen.getByTest(/welcome/i);
   expect(welcome).toBeInTheDocument();
   screen.debug();
