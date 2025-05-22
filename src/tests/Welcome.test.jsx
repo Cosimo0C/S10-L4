f75c0d13897236d055652b85f16fd41101a8ca42
Cosimo0C <cosimocrupi01@gmail.com>
2025-05-22 17:06:20 +0200

    S10-L4

--- src/tests/Welcome.test.jsx
+++
@@ -1,4 +0,0 @@
-import { describe, it, expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import App from "../App";
-import Welcome from "../components/Welcome";
